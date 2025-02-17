e48eb75c9988938620e381b49d5c9737842c6ebf
Thais Machado <thaiscristianemachado@hotmail.com>
2025-02-17 20:05:55 -0300

    Criando métodos para sacar e depositar

--- src/conta/model/Conta.java
+++ src/conta/model/Conta.java
@@ -56,6 +56,46 @@ public class Conta {
 		this.saldo = saldo;
 	}
 	
+	public boolean sacar(float valor) {
+		
+		if(this.getSaldo() < valor) {
+			System.out.println("\n Saldo Insuficiente!");
+			return false;
+		}
+		
+		this.setSaldo(this.getSaldo() - valor);
+		return true;	
+	}
+	
+	public void depositar(float valor) {
+		
+		this.setSaldo(this.getSaldo() + valor);
+	}
+	
+	public void visualizar() {
+		
+		String tipo = "";
+		
+		switch(this.tipo){
+		case 1:
+			tipo = "Conta Corrente";
+			break;
+		case 2:
+			tipo = "Conta Poupança";
+			break;
+		}
+		
+		System.out.println("\n\n*********************************************************");
+		System.out.println("Dados da Conta:");
+		System.out.println("*************************************************************");
+		System.out.println("Número da Conta: " + this.numero);
+		System.out.println("Agência: " + this.agencia);
+		System.out.println("Tipo da Conta: " + tipo);
+		System.out.println("Titular: " + this.titular);
+		System.out.println("Saldo: " + this.saldo);
+		
+	}
+	
 	
 
 }
