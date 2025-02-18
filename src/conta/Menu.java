7f4d7af583180f15cb3593d3b2e5a47b4bb3b4bb
Thais Machado <thaiscristianemachado@hotmail.com>
2025-02-17 23:26:52 -0300

    Criação do package repository e da classe ContaRepository

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,7 +1,6 @@
 package conta;
 
 import java.util.Scanner;
-import conta.model.Conta;
 import conta.model.ContaCorrente;
 import conta.model.ContaPoupanca;
 import conta.util.Cores;
@@ -9,13 +8,7 @@ import conta.util.Cores;
 public class Menu {
 
 	public static void main(String[] args) {
-		// Teste da Classe Conta
-				Conta c1 = new Conta(3, 123, 1, "Mariana", 500000.0f);
-				c1.visualizar();
-				c1.sacar(12000.0f);
-				c1.visualizar();
-				c1.depositar(5000.0f);
-				c1.visualizar();
+	
 		//Teste da Classe Conta Corrente 		
 				ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
 				cc1.visualizar();
